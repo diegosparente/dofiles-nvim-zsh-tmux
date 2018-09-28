a1e94203df2ad64bb5d26b5b59b3d6687f3ba574
diegosparente <diego.s.parente@gmail.com>
2018-09-28 14:55:32 -0300

    updated config.fish

--- extra/config.fish
+++ extra/config.fish
@@ -11,6 +11,7 @@ alias dcup='docker-compose up'
 alias dcrun='docker-compose run --rm app'
 alias dclogs='docker-compose logs'
 alias dcbuild='docker-compose build'
+alias nvimc='nvim ~/.config/nvim/init.vim'
 
 # alias List
 alias l='la'
@@ -20,7 +21,7 @@ alias pyserver='python -m SimpleHTTPServer'
 
 # Android-studio/React-Native | SDK
 
-set -lx ANDROID_HOME $HOME/Android/Sdk
+set --export ANDROID_HOME $HOME/Android/Sdk
 set -Ux PATH $ANDROID_HOME/tools $PATH
 set -Ux PATH $ANDROID_HOME/tools/bin $PATH
 set -Ux PATH $ANDROID_HOME/platform-tools $PATH
